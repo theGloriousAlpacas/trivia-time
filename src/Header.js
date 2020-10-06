bb49c467d10cd9c1c6aab463b9e319c7b0be5a07
anjelicatizon <anjelicatizon@gmail.com>
2020-10-05 20:33:25 -0400

    background styling

--- src/Header.js
+++ src/Header.js
@@ -9,8 +9,7 @@ const Header = () => {
             <div className="wrapper">
                 <div className="headerContainer">
                     <img src={logo} className="logo" alt="Trivia Time logo"/>
-                    <h1>Trivia Time</h1>
-                    <p>Fight your friends in a battle of wits!</p>
+                    <h1>Fight your friends in a battle of wits!</h1>
                 </div>
             </div>
         </header>
