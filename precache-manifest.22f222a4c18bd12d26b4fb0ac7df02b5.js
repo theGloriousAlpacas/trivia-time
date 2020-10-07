707368feff9dc810bd9b2eb3082c1622b2a35654
anjelicatizon <anjelicatizon@gmail.com>
2020-10-07 14:29:37 -0400

    Updates

---
+++ precache-manifest.22f222a4c18bd12d26b4fb0ac7df02b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b7b94e32acbe217ce46923118d5b85",
+    "url": "/trivia-time/index.html"
+  },
+  {
+    "revision": "d273ba5ddd45b86c650f",
+    "url": "/trivia-time/static/css/main.2bed5f87.chunk.css"
+  },
+  {
+    "revision": "bbbb4ed3f25f0a8a99f8",
+    "url": "/trivia-time/static/js/2.8036cd36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trivia-time/static/js/2.8036cd36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d273ba5ddd45b86c650f",
+    "url": "/trivia-time/static/js/main.b0041289.chunk.js"
+  },
+  {
+    "revision": "57410d5c0e475350cf98",
+    "url": "/trivia-time/static/js/runtime-main.b57ad402.js"
+  },
+  {
+    "revision": "aff3404ee611b7db45f129a456824a83",
+    "url": "/trivia-time/static/media/triviaTimeLogo.aff3404e.png"
+  }
+]);
