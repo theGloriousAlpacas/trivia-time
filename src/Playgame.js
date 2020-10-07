16794ea9851a1fc68454599454707c818c080363
anjelicatizon <anjelicatizon@gmail.com>
2020-10-06 20:57:54 -0400

    updated timer functionality plus pseudo code for playgame.js

--- src/Playgame.js
+++ src/Playgame.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Timer from './Timer';
 
 // Started the current game at current Player at 0 which is the first player.
 // The current question for the player is 0 too.
 class Playgame extends Component {
 
+    // Setting state to understand which player is playing and which question is showing up
     constructor() {
         super();
         this.state = {
@@ -12,17 +14,23 @@ class Playgame extends Component {
         };
     }
 
+    // Checking if the answer is right or wrong
     onAnswerClicked = (question, answer) => {
         console.log(question, answer);
+        // The API has a correctanswer property so we are checking if the answer they clicked is === to the correctanswer
         console.log(answer === question.correct_answer)
+        // Need to push correct answer to score in state
 
         let nextPlayer = this.state.currentPlayer;
+        // increments the questions up by 1 index number
         let nextQuestionPosition = this.state.currentQuestion + 1;
+        // if this.state.currentQuestion is greater than an index of 2, reset this.state.currentQuestion to 0 and increment to the next player index (go to the next player)
         if (nextQuestionPosition > 2) {
             nextQuestionPosition = 0;
             nextPlayer = nextPlayer + 1;
         }
 
+        // 
         this.setState({
             currentPlayer: nextPlayer,
             currentQuestion: nextQuestionPosition
@@ -40,28 +48,49 @@ class Playgame extends Component {
         }
     }
 
+    // This function is called right on page load
     showQuestions = () => {
-        console.log(this.props.players)
+        // console.log(this.props.players)
+
+        // ERROR HANDLING: If there are no players (this.state.players) OR no questions in the players state, don't render anything
         if (!this.props.players || !this.props.players[0].questions) {
             console.log("Pick your categories")
             return <></>
+
         } else {
-            console.log(this.props.players)
+            // console.log(this.props.players)
+            // ELSE look in the players array for the current player's index number
             const player = this.props.players[this.state.currentPlayer];
 
+            // If there is no player number, display "no player"
+                // This is at the end of the game when there are no more players to play - this where we'll route to the scoreboard
             if (!player) {
-                return <div>No player</div>
+                return <div>No player</div> 
             }
 
+            // question = the player's current question
             const question = player.questions[this.state.currentQuestion];
             // console.log(question)
+
+            // This show all 4 answers brought back from the API - 3 incorrect and 1 correct
+                // 3 incorrect answers are in an array so we are spreading it
             const allAnswers = [...question.incorrect_answers, question.correct_answer]
+
+            // Shuffling the answers using the shuffleArray method so the last answer isn't always the right one
             this.shuffleArray(allAnswers)
+
             return <div>
-                <p>Player {player.name}</p>
+                {/* Shows current player's name they entered */}
+                <p>Player {player.name}</p> 
+
+                {/* Question #: question text from API call */}
                 <h2>{`Question ${this.state.currentQuestion + 1} : ${question.question}`}</h2>
+
+                {/* Rendering all answers on the page as buttons */}
                 {allAnswers.map((answer) => {
                     // console.log(this);
+                    // When a button is clicked, run onAnswerClicked function which checks if it's right or wrong
+                        // first param is the question, second is an answer param
                     return <button onClick={() => this.onAnswerClicked(question, answer)}>
                         {answer}
                     </button>
@@ -72,14 +101,10 @@ class Playgame extends Component {
     }
 
     render() {
-        // return this.showQuestions();
         return (
             <div>
-
-                {this.props.title}
-                <h1>PLAYYYYY</h1>
+                {this.showQuestions()}
             </div>
-
         )
 
 
--- src/Timer.js
+++ src/Timer.js
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
-
-
     // Stretch goal - change color/appearance when at 10 seconds and then at 5 seconds to visually show you're running out of time
-
 class Timer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            timer: 30
+            timer: 30,
+            start: true
         }
     }
-
     // Create a timer that counts down from 30 seconds - needs to be tied to the Category Component so once category is picked, timer starts
-    componentDidMount(){
-        this.myInterval = setInterval( () => {
-            this.setState({
-                timer: this.state.timer - 1
-            })
-        }, 1000)
-    }
-
+    // componentDidMount(){
+    //     this.myInterval = setInterval( () => {
+    //         this.setState({
+    //             timer: this.state.timer - 1
+    //         })
+    //     }, 1000)
+    // }
     // ComponentDidUpdate kicks in when state is changed
         // Score state will update which will trigger this - check Category.js
     componentDidUpdate(){
@@ -27,16 +23,26 @@ class Timer extends Component {
          if (this.state.timer === 0) {
             // set timer back to 30
             this.setState({
-              timer: 30,
+                timer: 30,
+                start: false
             })
-
+            clearInterval();
             //Then go to the next question
           }
     }
-
+    startQuiz = (e) => {
+        e.preventDefault();
+        this.setState({
+            start: true
+        })
+        this.myInterval = setInterval( () => {
+            this.setState({
+                timer: this.state.timer - 1
+            })
+        }, 1000)
+    }
     render() {
         // let timer = this.state.timer
-
         // Setting styles for timer
         let timer = () => {   
             if (this.state.timer >= 15) {
@@ -47,14 +53,75 @@ class Timer extends Component {
                 return <p className="timerDanger">{this.state.timer}</p>
             }
         }
-
         return (
             <>
                 <h1>Hello this is the Timer</h1>
-                <p>{timer()}</p>
+                <button onClick={this.startQuiz}>Start Quiz</button>
+                <p>{this.state.start === true ? timer() : "Time's Up!"}</p>
             </>
         )
     }
 }
-
 export default Timer;
+
+
+// import React, { Component } from "react";
+
+
+//     // Stretch goal - change color/appearance when at 10 seconds and then at 5 seconds to visually show you're running out of time
+
+// class Timer extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             timer: 30
+//         }
+//     }
+
+//     // Create a timer that counts down from 30 seconds - needs to be tied to the Category Component so once category is picked, timer starts
+//     componentDidMount(){
+//         this.myInterval = setInterval( () => {
+//             this.setState({
+//                 timer: this.state.timer - 1
+//             })
+//         }, 1000)
+//     }
+
+//     // ComponentDidUpdate kicks in when state is changed
+//         // Score state will update which will trigger this - check Category.js
+//     componentDidUpdate(){
+//          // What happens when the timer hits 0
+//          if (this.state.timer === 0) {
+//             // set timer back to 30
+//             this.setState({
+//               timer: 30,
+//             })
+
+//             //Then go to the next question
+//           }
+//     }
+
+//     render() {
+//         // let timer = this.state.timer
+
+//         // Setting styles for timer
+//         let timer = () => {   
+//             if (this.state.timer >= 15) {
+//                 return <p className="timerOk">{this.state.timer}</p>
+//             } else if (this.state.timer >= 10) {
+//                 return <p className="timerWrapUp">{this.state.timer}</p>
+//             } else {
+//                 return <p className="timerDanger">{this.state.timer}</p>
+//             }
+//         }
+
+//         return (
+//             <>
+//                 <h1>Hello this is the Timer</h1>
+//                 <p>{timer()}</p>
+//             </>
+//         )
+//     }
+// }
+
+// export default Timer;
